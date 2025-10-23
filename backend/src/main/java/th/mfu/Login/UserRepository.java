740d5ba6f77919e34cddde1148b75bdece226f7c
Kanthi6731503004 <6731503004@lamduan.mfu.ac.th>
2025-10-23 09:56:17 +0000

    6731503004

--- backend/src/main/java/th/mfu/Login/UserRepository.java
+++ backend/src/main/java/th/mfu/Login/UserRepository.java
@@ -1,3 +1,9 @@
+package java.th.mfu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByStudentId(String studentId);
