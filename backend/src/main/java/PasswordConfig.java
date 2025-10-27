9eb89295adecdad7c4ad542eafeb20c612cfadf8
Wacharaphong Sutthuboriban <6731503032@lamduan.mfu.ac.th>
2025-10-27 19:01:11 +0700

    6731503032

--- backend/src/main/java/PasswordConfig.java
+++ backend/src/main/java/PasswordConfig.java
@@ -1,5 +1,3 @@
-package th.mfu.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
--- backend/target/classes/user.sql
+++ backend/target/classes/user.sql
@@ -1,5 +1,7 @@
-INSERT INTO users (student_id, name, email, password, role, is_active, created_at, updated_at) 
-VALUES ('6731503032', 'Wacharaphong Sutthiboriban', '6731503032@lamduan.mfu.ac.th', 'password', 'STUDENT', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (student_id, name, email, password, role, is_active, created_at, updated_at)
+VALUES 
+('6731503032', 'Wacharaphong Sutthiboriban', '6731503032@lamduan.mfu.ac.th', 'password', 'STUDENT', TRUE, NOW(), NOW());
 
-INSERT INTO users (student_id, name, email, password, role, is_active, created_at, updated_at) 
-VALUES ('ADMIN001', 'Manager', 'admin@example.com', 'adminpassword', 'MANAGER', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (student_id, name, email, password, role, is_active, created_at, updated_at)
+VALUES 
+('ADMIN001', 'Manager', 'admin@example.com', 'adminpassword', 'MANAGER', TRUE, NOW(), NOW());
