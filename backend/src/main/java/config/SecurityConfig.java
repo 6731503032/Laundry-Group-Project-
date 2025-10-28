ba75dd67efd87f037c8ac869544135d2e4b3246c
Kanthi6731503004 <6731503004@lamduan.mfu.ac.th>
2025-10-28 14:01:19 +0000

    Fix repeating login

--- backend/src/main/java/config/SecurityConfig.java
+++ backend/src/main/java/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
         http
             .csrf().disable()
             .authorizeRequests()
-                .antMatchers("/", "/login", "/register", "/booking", "/rating", "/timer", "/api/auth/**", "/h2-console/**").permitAll()
+                .antMatchers("/", "/login", "/register", "/booking", "/rating", "/timer", "/student/dashboard", "/manager/dashboard", "/api/auth/**", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .httpBasic();
